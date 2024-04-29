dc3890151e9c2c19788d9cea185b6619ca814af8
bobbepallisri <srikanthbobbepalli3@gmail.com>
2024-04-29 10:16:06 +0000

    solution

---
+++ src/components/CryptocurrencyTracker/index.js
@@ -0,0 +1,16 @@
+// Write your code here
+import {Component} from 'react'
+import './index.css'
+import CryptocurrenciesList from '../CryptocurrenciesList'
+
+class CryptocurrencyTracker extends Component {
+  render() {
+    return (
+      <div className="main-container">
+        <CryptocurrenciesList />
+      </div>
+    )
+  }
+}
+
+export default CryptocurrencyTracker
